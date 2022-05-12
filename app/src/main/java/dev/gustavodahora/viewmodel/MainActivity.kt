d1bd0d47cc96a1ef0b51081114b4e1d5c5422d5f
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-05-12 02:46:11 -0300

    final commit and simpe use of ViewModel on Kotlin

--- app/src/main/java/dev/gustavodahora/viewmodel/MainActivity.kt
+++ app/src/main/java/dev/gustavodahora/viewmodel/MainActivity.kt
@@ -2,10 +2,26 @@ package dev.gustavodahora.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import dev.gustavodahora.viewmodel.databinding.ActivityMainBinding
+import dev.gustavodahora.viewmodel.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.tvIncreasingNumber.text = viewModel.number.toString()
+
+        binding.btnIncreaseButton.setOnClickListener {
+            viewModel.addNumber()
+            binding.tvIncreasingNumber.text = viewModel.number.toString()
+        }
     }
 }
---
+++ app/src/main/java/dev/gustavodahora/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,10 @@
+package dev.gustavodahora.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+    var number = 0
+    fun addNumber() {
+        number++
+    }
+}
